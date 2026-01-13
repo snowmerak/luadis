d7e88616fede37f3b68227889c791064cc3c0e08
snowmerak <snowmerak@outlook.com>
2026-01-14 01:20:58 +0900

    feat(client): implement New function for redis client creation

--- client.go
+++ client.go
@@ -1,7 +1,26 @@
 package luadis
 
-import "github.com/redis/rueidis"
+import (
+	"fmt"
+
+	"github.com/redis/rueidis"
+)
+
+type Options = rueidis.ClientOption
 
 type Client struct {
 	conn rueidis.Client
+}
+
+func New(opt Options) (*Client, error) {
+	conn, err := rueidis.NewClient(opt)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create redis client: %w", err)
+	}
+
+	return &Client{conn: conn}, nil
+}
+
+func With(conn rueidis.Client) *Client {
+	return &Client{conn: conn}
 }
