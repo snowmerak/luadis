3963fe6ece8352c22eb23ca1aa11b015790ee67d
snowmerak <snowmerak@outlook.com>
2026-01-14 01:41:52 +0900

    refactor(script): update Exec method to return error instead of response

--- script.go
+++ script.go
@@ -26,16 +26,15 @@ func NewScript[Req ScriptRequest, Res ScriptResponse](script string) *Script[Req
 	}
 }
 
-func (s *Script[Req, Res]) Exec(ctx context.Context, client *Client, request Req) (Res, error) {
-	var zero Res
+func (s *Script[Req, Res]) Exec(ctx context.Context, client *Client, request Req, dest Res) error {
 	result := s.script.Exec(ctx, client.conn, request.Keys(), request.Args())
 	if err := result.Error(); err != nil {
-		return zero, fmt.Errorf("failed to execute script: %w", err)
+		return fmt.Errorf("failed to execute script: %w", err)
 	}
 
-	if err := zero.FromReply(result); err != nil {
-		return zero, fmt.Errorf("failed to parse script response: %w", err)
+	if err := dest.FromReply(result); err != nil {
+		return fmt.Errorf("failed to parse script response: %w", err)
 	}
 
-	return zero, nil
+	return nil
 }
